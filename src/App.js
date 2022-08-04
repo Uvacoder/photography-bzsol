8cb6882e33cb6f5f253b973190cc75d2222cd126
bzsol <bereczzsolt@outlook.com>
2022-08-04 16:19:57 +0200

    Added gallery

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { Gallery } from "react-photoswipe-gallery";
 import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import About from "./Pages/About";
 import Home from "./Pages/Home";
 import Error from "./Pages/Error";
+import Wedding from "./Pages/Wedding";
 
 function App() {
   
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
-        <Route path="/gallery" element={<Gallery/>} />
+        <Route path="/wedding" element={<Wedding/>} />
         <Route path="*" element={<Error/>}  />
       </Routes>
     </Router>
