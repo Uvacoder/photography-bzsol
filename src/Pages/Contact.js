4749b4f2fead046ea4c5ad1fcc8f1e47e5325be9
bzsol <bereczzsolt@outlook.com>
2022-08-05 14:43:13 +0200

    Added contact

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -9,6 +9,10 @@ const Contact = () => {
             <Navbar></Navbar>
             <div className="container flex flex-col md:flex-row items-center px-6 mx-auto mt-2 space-y-0 md:space-y-0 p-12">
                 <div className="container mx-10 md:mx-96 my-40 p-6 max-w-md bg-white rounded-lg border border-gray-200 shadow-2xl w-4/5 md:w-1/2 ">
+                <div className="mb-3">
+                <h1 className='text-2xl font-bold text-red-500 mb-2'>E-mail</h1>
+                <a href="mailto:svetz" className="hover:text-sky-500 font-semibold">boss@svetz.hu</a>
+                </div>
                     <form className="flex flex-col">
                         <label className="block" >
                             <span className="block after:content-['*'] after:ml-0.5 after:text-red-500 text-sm font-medium text-slate-700">Név:</span>
@@ -26,31 +30,32 @@ const Contact = () => {
                             <span className="block text-sm font-medium text-slate-700">Üzenet:</span>
                             <textarea name="body" className="mt-1 block w-full px-3 py-3 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-noneinvalid:border-pink-500 invalid:text-pink-600focus:invalid:border-pink-500 focus:invalid:ring-pink-500" />
                         </label>
-                        <button type="submit" className="relative text-center  mt-10 bg-sky-500 hover:bg-sky-600 focus:outline-none focus:ring focus:ring-sky-300 active:bg-sky-700 px-10 py-2 text-sm leading-5 rounded-lg font-semibold text-white "> Üzenet küldése</button>
+                        <div className="flex flex-col">
+                        <button type="submit" className="relative text-center  mt-5 bg-sky-500 hover:bg-sky-600 focus:outline-none focus:ring focus:ring-sky-300 active:bg-sky-700 px-10 py-2 text-sm leading-5 rounded-lg font-semibold text-white "> Üzenet küldése</button>
+                        <button className="relative text-center  mt-1 bg-red-500 hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-300 active:bg-red-700 px-10 py-2 text-sm leading-5 rounded-lg font-semibold text-white "><a href="mailto:svetz@svetz.hu">Direkt Email írása</a></button>
+                        </div>
+                        
                     </form>
                 </div>
                 <div className="flex flex-col items-center w-1/5  md:w-1/2 ">
+                <h1 className='text-2xl md:text-6xl font-bold text-center text-slate-600 mb-10'>Social Media</h1>
                     <div className="flex flex-row ">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" className="bi bi-facebook px-6 fill-sky-500 hover:fill-sky-600 focus:outline-none focus:ring focus:ring-sky-300 active:fill-sky-700 " viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" className="bi bi-facebook px-7 md:px-5 fill-sky-500 hover:fill-sky-600 focus:outline-none focus:ring focus:ring-sky-300 active:fill-sky-700 " viewBox="0 0 16 16">
                             <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
                         </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" className="bi bi-instagram px-6 fill-violet-500 hover:fill-violet-600 focus:outline-none focus:ring focus:ring-violet-300 active:fill-violet-700" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" className="bi bi-instagram px-7 md:px-5 fill-violet-500 hover:fill-violet-600 focus:outline-none focus:ring focus:ring-violet-300 active:fill-violet-700" viewBox="0 0 16 16">
                             <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
                         </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" className="bi bi-whatsapp px-6 fill-green-500 hover:fill-green-600 focus:outline-none focus:ring focus:ring-green-300 active:fill-green-700" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" className="bi bi-whatsapp px-7 md:px-5 fill-green-500 hover:fill-green-600 focus:outline-none focus:ring focus:ring-green-300 active:fill-green-700" viewBox="0 0 16 16">
                             <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
                         </svg>
                         <button data-tooltip-target="tooltip-default" type="button">
 
-                            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-reddit  px-6 fill-orange-500 hover:fill-orange-600 focus:outline-none focus:ring focus:ring-orange-300 active:fill-orange-700" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-reddit px-7  md:px-5 fill-orange-500 hover:fill-orange-600 focus:outline-none focus:ring focus:ring-orange-300 active:fill-orange-700" viewBox="0 0 16 16">
                                 <path d="M6.167 8a.831.831 0 0 0-.83.83c0 .459.372.84.83.831a.831.831 0 0 0 0-1.661zm1.843 3.647c.315 0 1.403-.038 1.976-.611a.232.232 0 0 0 0-.306.213.213 0 0 0-.306 0c-.353.363-1.126.487-1.67.487-.545 0-1.308-.124-1.671-.487a.213.213 0 0 0-.306 0 .213.213 0 0 0 0 .306c.564.563 1.652.61 1.977.61zm.992-2.807c0 .458.373.83.831.83.458 0 .83-.381.83-.83a.831.831 0 0 0-1.66 0z" />
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.828-1.165c-.315 0-.602.124-.812.325-.801-.573-1.9-.945-3.121-.993l.534-2.501 1.738.372a.83.83 0 1 0 .83-.869.83.83 0 0 0-.744.468l-1.938-.41a.203.203 0 0 0-.153.028.186.186 0 0 0-.086.134l-.592 2.788c-1.24.038-2.358.41-3.17.992-.21-.2-.496-.324-.81-.324a1.163 1.163 0 0 0-.478 2.224c-.02.115-.029.23-.029.353 0 1.795 2.091 3.256 4.669 3.256 2.577 0 4.668-1.451 4.668-3.256 0-.114-.01-.238-.029-.353.401-.181.688-.592.688-1.069 0-.65-.525-1.165-1.165-1.165z" />
                             </svg>
                         </button>
-                        <div id="tooltip-default" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
-                            Tooltip content
-                            <div class="tooltip-arrow" data-popper-arrow></div>
-                        </div>
                     </div>
                 </div>
             </div>
