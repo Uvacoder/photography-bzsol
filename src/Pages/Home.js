ffdafdec69b76c4be734861c5f3d059c00333bb8
bzsol <bereczzsolt@outlook.com>
2022-09-10 13:28:59 +0200

    added review delete

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from "../Components/NavbarFrontPage";
 import Hero from "../Components/Hero";
 import About from "../Components/About";
-import Reviews from "../Components/Reviews";
 import Footer from "../Components/Footer";
 import Options from '../Components/Options';
 import Contact from '../Components/Contact';
