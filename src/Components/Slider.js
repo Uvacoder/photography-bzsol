6f9588ce1d6665db20dbdb4d81fc26793696a0dc
bzsol <bereczzsolt@outlook.com>
2022-09-10 12:53:16 +0200

    Updated slider

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -5,7 +5,6 @@ import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation } from "swiper";
 import portre2 from '../img/slider/portre2.jpg';
 import portre from '../img/slider/portre.jpg';
-import boys from '../img/slider/boys.jpg';
 import enterior from '../img/slider/enterior.jpg';
 import wedding from '../img/slider/wedding.jpg';
 import wedding2 from '../img/slider/wedding2.jpg';
@@ -27,12 +26,11 @@ const Slider = () => {
         modules={[Autoplay, Pagination, Navigation]}
         className="rounded-lg shadow-2xl"
       >
-        <SwiperSlide><img src={portre2} width={800} height={800} alt="Potré" ></img></SwiperSlide>
-        <SwiperSlide><img src={wedding2} width={800} height={800} alt="Portré" ></img></SwiperSlide>
-        <SwiperSlide><center><img src={boys} width={330} height={300} alt="Potré" ></img></center></SwiperSlide>
-        <SwiperSlide><img src={portre} width={800} height={800} alt="Portré" ></img></SwiperSlide>
-        <SwiperSlide><img src={wedding} width={800} height={800} alt="Portré" ></img></SwiperSlide>
-        <SwiperSlide><img src={enterior} width={800} height={800} alt="Portré" ></img></SwiperSlide>
+        <SwiperSlide><img src={portre2} width={800} height={800} alt="Potré fotók" ></img></SwiperSlide>
+        <SwiperSlide><img src={wedding2} width={800} height={800} alt="Esküvői fotók" ></img></SwiperSlide>
+        <SwiperSlide><img src={portre} width={800} height={800} alt="Potré fotók" ></img></SwiperSlide>
+        <SwiperSlide><img src={wedding} width={800} height={800} alt="Esküvői fotók" ></img></SwiperSlide>
+        <SwiperSlide><img src={enterior} width={800} height={800} alt="Enteriőr fotók" ></img></SwiperSlide>
       </Swiper>
     );
 }
